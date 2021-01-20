10f5e08bf243823176e969101a8be03d729ec628
EC2 Default User <ec2-user@ip-172-31-84-131.ec2.internal>
2021-01-20 01:13:52 +0000

    average of mark

---
+++ average_of_mark.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved.
+//
+// Created by: Donggeun Lim
+// Created on: Jan 2021
+// This program calculates average of mark
+
+
+#include <iostream>
+#include <list>
+
+
+main() {
+    // this function uses a list to calculate the average of marks
+
+    std::list<float> marks;
+    float tempMark;
+    float average;
+    float total = 0;
+    float counter = 0;
+
+    // input
+    std::cout << "Please enter 1 mark at a time. Enter -1 to end.";
+    std::cout << std::endl;
+
+    std::cout << "What is your mark? (as %): ";
+    std::cin >> tempMark;
+    marks.push_back(tempMark);
+
+    while (tempMark != -1) {
+        std::cout << "What is your mark? (as %): ";
+        std::cin >> tempMark;
+        marks.push_back(tempMark);
+    }
+
+    marks.pop_back();  // remove the "-1" that was added
+    std::cout << "" << std::endl;
+
+    for (float tempMark2 : marks) {
+        marks.push_front(tempMark2);
+        total += tempMark2;
+        counter += 1;
+    }
+
+    average = total / (counter);
+
+    std::cout << "The average is: " << average << std::endl;
+    std::cout << std::endl;
+}
